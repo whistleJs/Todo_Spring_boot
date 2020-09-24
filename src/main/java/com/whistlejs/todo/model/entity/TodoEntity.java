54c9dff0b7201c880e8a669c69ba03c02ea7f4fd
whistleJs <whistle_js@naver.com>
2020-09-24 23:47:08 +0900

    2020-09-24 TodoEntity 생성

---
+++ src/main/java/com/whistlejs/todo/model/entity/TodoEntity.java
@@ -0,0 +1,38 @@
+package com.whistlejs.todo.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "todo")
+public class TodoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = false)
+    @JsonIgnore
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    @JsonIgnore
+    private LocalDateTime deletedAt = null;
+
+}

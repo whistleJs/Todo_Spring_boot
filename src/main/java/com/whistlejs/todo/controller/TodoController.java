0c6b0a5a57ee262be510ffcf2ea9236f58c4973e
whistleJs <whistle_js@naver.com>
2020-09-26 00:27:09 +0900

    2020-09-26 Todo 생성하기

--- src/main/java/com/whistlejs/todo/controller/TodoController.java
+++ src/main/java/com/whistlejs/todo/controller/TodoController.java
@@ -1,13 +1,25 @@
 package com.whistlejs.todo.controller;
 
+import com.whistlejs.todo.model.entity.TodoEntity;
 import com.whistlejs.todo.service.TodoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/todo")
 @RestController
 public class TodoController {
     @Autowired
     TodoService service;
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @PostMapping
+    public ResponseEntity<TodoEntity> create(@RequestBody TodoEntity todo) {
+        logger.info(todo.getContent(), todo.getTitle());
+        return new ResponseEntity<TodoEntity>(service.create(todo), HttpStatus.OK);
+    }
 }
--- src/main/java/com/whistlejs/todo/service/TodoService.java
+++ src/main/java/com/whistlejs/todo/service/TodoService.java
@@ -1,5 +1,6 @@
 package com.whistlejs.todo.service;
 
+import com.whistlejs.todo.model.entity.TodoEntity;
 import com.whistlejs.todo.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,8 @@ import org.springframework.stereotype.Service;
 public class TodoService {
     @Autowired
     TodoRepository repository;
+
+    public TodoEntity create(TodoEntity todo) {
+        return repository.save(todo);
+    }
 }
